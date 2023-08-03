c0a8d5192ea5f2fea5311cc6cefd3f4b6ca54d50
olle1134 <99058610+olle1134@users.noreply.github.com>
2023-08-03 14:39:34 +1200

    Began styling in ButtonContainer, rearranged button elements to resemble the wire frame lay out Adjusted css rules to do the same

--- RN-calculator/src/components/ButtonContainer.js
+++ RN-calculator/src/components/ButtonContainer.js
@@ -3,184 +3,137 @@ import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 
 export default function ButtonContainer({ onButton, onClear, onDelete }) {
   return (
-    <>
-      <View style={styles.btnContainer}>
-        <View style={styles.numericBtnContainer}>
-          <View style={styles.buttonGroup}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("1")}
-            >
-              <Text>1</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("2")}
-            >
-              <Text>2</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("3")}
-            >
-              <Text>3</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.buttonGroup}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("4")}
-            >
-              <Text>4</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("5")}
-            >
-              <Text>5</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("6")}
-            >
-              <Text>6</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.buttonGroup}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("7")}
-            >
-              <Text>7</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("8")}
-            >
-              <Text>8</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("9")}
-            >
-              <Text>9</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.buttonGroup}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("0")}
-            >
-              <Text>0</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton(".")}
-            >
-              <Text>.</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={[styles.button, { backgroundColor: "blue" }]}
-              onPress={() => onButton("=")}
-              testID="equalsButton" // Add testID prop here
-            >
-              <Text>=</Text>
-            </TouchableOpacity>
-          </View>
-          {/* Operator Buttons */}
-          <View style={styles.operatorButtonContainer}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("+")}
-            >
-              <Text style={styles.buttonText}>+</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("-")}
-            >
-              <Text style={styles.buttonText}>-</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("*")}
-            >
-              <Text style={styles.buttonText}>*</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => onButton("/")}
-            >
-              <Text style={styles.buttonText}>/</Text>
-            </TouchableOpacity>
-          </View>
+    <View style={styles.btnContainer}>
+      <View style={styles.numericBtnContainer}>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("7")}>
+            <Text>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("8")}>
+            <Text>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("9")}>
+            <Text>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("/")}>
+            <Text style={styles.buttonText}>/</Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.calculatorControls}>
-          <TouchableOpacity style={styles.controlButton} onPress={onDelete}>
-            <Text>Delete</Text>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("4")}>
+            <Text>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("5")}>
+            <Text>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("6")}>
+            <Text>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("*")}>
+            <Text style={styles.buttonText}>*</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("1")}>
+            <Text>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("2")}>
+            <Text>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("3")}>
+            <Text>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("-")}>
+            <Text style={styles.buttonText}>-</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("0")}>
+            <Text>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => onButton(".")}>
+            <Text>.</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            style={[styles.controlButton, { backgroundColor: "red" }]}
-            onPress={onClear}
+            style={[styles.button, { backgroundColor: "red" }]}
+            onPress={onDelete}
           >
-            <Text>Clear history</Text>
+            <Text>Delete</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => onButton("+")}>
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: "blue" }]}
+            onPress={() => onButton("=")}
+            testID="equalsButton"
+          >
+            <Text style={styles.buttonText}>=</Text>
           </TouchableOpacity>
         </View>
       </View>
-    </>
+      <View style={styles.calculatorControls}>
+        <TouchableOpacity style={styles.controlButton} onPress={onClear}>
+          <Text>Clear history</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   btnContainer: {
-    //  container styles
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+    paddingHorizontal: 20,
   },
   numericBtnContainer: {
-    //  numeric button container styles
+    flexDirection: "column",
+    justifyContent: "space-around",
+    marginBottom: 10,
   },
   buttonGroup: {
-    //  button group styles
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 10,
+  },
+  buttonRow: {
     flexDirection: "row",
     justifyContent: "space-around",
     marginBottom: 10,
   },
   button: {
-    //  button styles
     width: 80,
     height: 80,
     borderRadius: 40,
-    backgroundColor: "lightgrey",
+    backgroundColor: "#DBD8AE",
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: 10,
   },
-  calculatorControls: {
-    //  calculator controls styles
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  controlButton: {
-    //  control button styles
-    width: 120,
-    height: 40,
-    borderRadius: 20,
-    backgroundColor: "orange",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    marginTop: 20,
-  },
-  numericButtonContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
+  buttonText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "black",
   },
   operatorButtonContainer: {
     flexDirection: "row",
     justifyContent: "center",
+    marginLeft: 40,
+    marginTop: 10,
+  },
+  controlButton: {
+    width: 120,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "#CA907E",
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 10,
   },
 });
