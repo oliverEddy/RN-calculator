c99e948515bcf3e2365a8c0a9f2c77e31b2372fd
olle1134 <99058610+olle1134@users.noreply.github.com>
2023-08-07 12:37:33 +1200

    Update test cases - Update test cases to be asynchronous - Test now use reacts waitFor function in conjunction with async and await - Tests all pass with no console errors

--- RN-calculator/App.test.js
+++ RN-calculator/App.test.js
@@ -5,11 +5,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage);
 
-import { render, fireEvent } from "@testing-library/react-native";
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
+
 import App from "./App";
 
 describe("Calculator", () => {
-  test("Calculator performs addition correctly", () => {
+  test("Calculator performs addition correctly", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("1"));
@@ -17,10 +18,12 @@ describe("Calculator", () => {
     fireEvent.press(getByText("2"));
     fireEvent.press(getByText("="));
 
-    expect(getByText("1 + 2 = 3.00")).toBeTruthy();
+    await waitFor(() => {
+      expect(getByText("1 + 2 = 3.00")).toBeTruthy();
+    });
   });
 
-  test("Calculator performs subtraction correctly", () => {
+  test("Calculator performs subtraction correctly", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("5"));
@@ -28,10 +31,12 @@ describe("Calculator", () => {
     fireEvent.press(getByText("3"));
     fireEvent.press(getByText("="));
 
-    expect(getByText("5 - 3 = 2.00")).toBeTruthy();
+    await waitFor(() => {
+      expect(getByText("5 - 3 = 2.00")).toBeTruthy();
+    });
   });
 
-  test("Calculator performs multiplication correctly", () => {
+  test("Calculator performs multiplication correctly", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("4"));
@@ -39,10 +44,12 @@ describe("Calculator", () => {
     fireEvent.press(getByText("3"));
     fireEvent.press(getByText("="));
 
-    expect(getByText("4 * 3 = 12.00")).toBeTruthy();
+    await waitFor(() => {
+      expect(getByText("4 * 3 = 12.00")).toBeTruthy();
+    });
   });
 
-  test("Calculator performs division correctly", () => {
+  test("Calculator performs division correctly", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("6"));
@@ -50,10 +57,12 @@ describe("Calculator", () => {
     fireEvent.press(getByText("2"));
     fireEvent.press(getByText("="));
 
-    expect(getByText("6 / 2 = 3.00")).toBeTruthy();
+    await waitFor(() => {
+      expect(getByText("6 / 2 = 3.00")).toBeTruthy();
+    });
   });
 
-  test("Calculator displays result with 2 decimal places", () => {
+  test("Calculator displays result with 2 decimal places", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("1"));
@@ -61,10 +70,12 @@ describe("Calculator", () => {
     fireEvent.press(getByText("3"));
     fireEvent.press(getByText("="));
 
-    expect(getByText("1 / 3 = 0.33")).toBeTruthy();
+    await waitFor(() => {
+      expect(getByText("1 / 3 = 0.33")).toBeTruthy();
+    });
   });
 
-  test("Calculator performs calculations with decimal numbers and 0", () => {
+  test("Calculator performs calculations with decimal numbers and 0", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("2"));
@@ -74,10 +85,12 @@ describe("Calculator", () => {
     fireEvent.press(getByText("0"));
     fireEvent.press(getByText("="));
 
-    expect(getByText("2.5 + 0 = 2.50")).toBeTruthy();
+    await waitFor(() => {
+      expect(getByText("2.5 + 0 = 2.50")).toBeTruthy();
+    });
   });
 
-  test("Ensures only one decimal point is allowed per operand", () => {
+  test("Ensures only one decimal point is allowed per operand", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("1"));
@@ -86,10 +99,12 @@ describe("Calculator", () => {
     fireEvent.press(getByText("."));
     fireEvent.press(getByText("3"));
 
-    expect(getByText("1.23 =")).toBeDefined();
+    await waitFor(() => {
+      expect(getByText("1.23 =")).toBeDefined();
+    });
   });
 
-  test("Calculator can display negative numbers", () => {
+  test("Calculator can display negative numbers", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("5"));
@@ -97,10 +112,12 @@ describe("Calculator", () => {
     fireEvent.press(getByText("8"));
     fireEvent.press(getByText("="));
 
-    expect(getByText("5 - 8 = -3.00")).toBeTruthy();
+    await waitFor(() => {
+      expect(getByText("5 - 8 = -3.00")).toBeTruthy();
+    });
   });
 
-  test("Calculations can only be performed on 2 numeric values with one operator", () => {
+  test("Calculations can only be performed on 2 numeric values with one operator", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("1"));
@@ -112,32 +129,42 @@ describe("Calculator", () => {
     fireEvent.press(getByText("3"));
     fireEvent.press(getByText("="));
 
-    expect(getByText("1 / 23 = 0.04")).toBeDefined();
+    await waitFor(() => {
+      expect(getByText("1 / 23 = 0.04")).toBeDefined();
+    });
   });
 
-  test("User can change the operator before calculating", () => {
+  test("User can change the operator before calculating", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("1"));
     fireEvent.press(getByText("+"));
     fireEvent.press(getByText("2"));
 
-    expect(getByText("1 + 2 =")).toBeDefined();
+    await waitFor(() => {
+      expect(getByText("1 + 2 =")).toBeDefined();
+    });
 
     fireEvent.press(getByText("-"));
 
-    expect(getByText("1 - 2 =")).toBeDefined();
+    await waitFor(() => {
+      expect(getByText("1 - 2 =")).toBeDefined();
+    });
 
     fireEvent.press(getByText("*"));
 
-    expect(getByText("1 * 2 =")).toBeDefined();
+    await waitFor(() => {
+      expect(getByText("1 * 2 =")).toBeDefined();
+    });
 
     fireEvent.press(getByText("/"));
 
-    expect(getByText("1 / 2 =")).toBeDefined();
+    await waitFor(() => {
+      expect(getByText("1 / 2 =")).toBeDefined();
+    });
   });
 
-  test("User can not complete a calculation with out first and second operands", () => {
+  test("User can not complete a calculation without first and second operands", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("1"));
@@ -145,10 +172,12 @@ describe("Calculator", () => {
     fireEvent.press(getByText("="));
     fireEvent.press(getByText("="));
 
-    expect(getByText("1 + =")).toBeDefined();
+    await waitFor(() => {
+      expect(getByText("1 + =")).toBeDefined();
+    });
   });
 
-  test("User should not be able to enter an operator before the first operand has been entered", () => {
+  test("User should not be able to enter an operator before the first operand has been entered", async () => {
     const { getByText } = render(<App />);
 
     fireEvent.press(getByText("+"));
@@ -157,10 +186,12 @@ describe("Calculator", () => {
     fireEvent.press(getByText("/"));
     fireEvent.press(getByText("1"));
 
-    expect(getByText("1 =")).toBeDefined();
+    await waitFor(() => {
+      expect(getByText("1 =")).toBeDefined();
+    });
   });
 
-  test("Clear history button removes past calculations from dropdown and local storage", () => {
+  test("Clear history button removes past calculations from dropdown and local storage", async () => {
     const { getByText, queryByText } = render(<App />);
 
     fireEvent.press(getByText("1"));
@@ -177,15 +208,18 @@ describe("Calculator", () => {
 
     fireEvent.press(getByText("Past Calculations:"));
 
-    expect(getByText("1 + 2 = 3.00")).toBeDefined();
-    expect(getByText("5 - 3 = 2.00")).toBeDefined();
+    await waitFor(() => {
+      expect(getByText("1 + 2 = 3.00")).toBeDefined();
+      expect(getByText("5 - 3 = 2.00")).toBeDefined();
+    });
 
     fireEvent.press(getByText("Clear History"));
 
-    expect(queryByText("1 + 2 = 3.00")).toBeNull();
-    expect(queryByText("5 - 3 = 2.00")).toBeNull();
+    await waitFor(() => {
+      expect(queryByText("1 + 2 = 3.00")).toBeNull();
+      expect(queryByText("5 - 3 = 2.00")).toBeNull();
+    });
 
-    // Verify that the "calculations" key is removed from the mockAsyncStorage
     expect(mockAsyncStorage.data["calculations"]).toBeUndefined();
   });
 });
